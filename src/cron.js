947e2aa1ea102d2b1e24bd029b1d180cec4f4fe0
grandmullah <collinsbiegon9@gmail.com>
2023-07-04 16:28:01 +0300

    update src

--- src/cron.js
+++ src/cron.js
@@ -2,7 +2,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const fs = require('fs');
 
-const credentials = '/Volumes/COLLINS/project/offChainWorker/X509-cert-3406721595276256674.pem'
+const credentials = '../X509-cert-3406721595276256674.pem'
 
 const client = new MongoClient('mongodb+srv://cluster0.0wni5o1.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
   sslKey: credentials,
