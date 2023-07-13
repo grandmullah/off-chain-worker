43836d82e4703da7ba64531289400399f7cff2f6
grandmullah <collinsbiegon9@gmail.com>
2023-07-13 21:43:26 +0300

    update

--- src/service.js
+++ src/service.js
@@ -81,6 +81,23 @@ const requestRide = async (req,res) => {
 module.exports = {
     requestRide
 }
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c * 1000; // Convert to meters
+  return distance;
+}
+
+// Function to convert degrees to radians
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
 //erase lucky memory ivory shiver position bleak will scrub unlock quantum direct
 // const calc =  (loc,item) =>{
    
